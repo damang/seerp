f9904b13a897a2a99440e8a0b441a2e43c6fbb2e
luisa.siniscalchi <luisa.siniscalchi@170c7fde-46e4-11de-aa63-c184e82abc01>
2009-06-12 14:03:37 +0000

    prova

---
+++ seerp/prova/src/Persona/Persona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Persona;
+
+/**
+ *
+ * @author Luisa
+ */
+public class Persona {
+    String nm;
+    String cg;
+     String nm1;
+
+    public String getCg() {
+        return cg;
+    }
+
+    public void setCg(String cg) {
+        this.cg = cg;
+    }
+
+    public String getCg1() {
+        return cg1;
+    }
+
+    public void setCg1(String cg1) {
+        this.cg1 = cg1;
+    }
+
+    public String getNm() {
+        return nm;
+    }
+
+    public void setNm(String nm) {
+        this.nm = nm;
+    }
+
+    public String getNm1() {
+        return nm1;
+    }
+
+    public void setNm1(String nm1) {
+        this.nm1 = nm1;
+    }
+    String cg1;
+
+}
---
+++ seerp/prova/src/prova/Main.java
@@ -0,0 +1,17 @@
+
+package prova;
+
+/**
+ *
+ * @author Luisa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
