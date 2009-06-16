2faf57831f8434b683fdacbe46aae0b225d03a15
giuseppe.russo87@gmail.com <giuseppe.russo87@gmail.com@170c7fde-46e4-11de-aa63-c184e82abc01>
2009-06-16 00:34:17 +0000

    ho aggiunto l'agenda di angelo!!

---
+++ seerp/src/it/seerp/bottoni/Nuovo/ButtonAgenda.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.seerp.bottoni.Nuovo;
+
+import configurazioni.CommandInterface;
+import it.seerp.Gui.Menu.MenuRuoli;
+import it.seerp.Gui.command.ObserverButton;
+import it.seerp.storage.ejb.Evento;
+import it.seerp.storage.operazioni.OpEvento;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import us.k5n.ui.calendar.CalendarDataRepository;
+import us.k5n.ui.calendar.CalendarPanel;
+import us.k5n.ui.calendar.Event;
+
+
+
+/**
+ *
+ * @author peppe
+ */
+public class ButtonAgenda extends ObserverButton implements CommandInterface{
+
+    protected JTabbedPane tabbedPane;
+    private MenuRuoli menu;
+    CalendarPanel panel;
+    String s = "Agenda";
+
+    /**
+     *
+     * @param gruppoFinestreUpdate
+     * @param aThis
+     */
+    public ButtonAgenda(JTabbedPane pan, MenuRuoli menu, ActionListener act) throws SQLException {
+        this.tabbedPane = pan;
+        this.menu = menu;
+        //this.panel = new AreaUtentePanel(TIPO_UTENTE_CONST.CLIENTE);
+
+    //panel.register(this);
+        this.addActionListener(act);
+
+
+    }
+
+    public ButtonAgenda() {
+        super();
+    }
+
+    public void execute() {
+
+        if (!isPresente) {
+           
+               this.panel = new CalendarPanel(new CalendarDataRepository() {
+                   public Vector getEventInstancesForDate(int year, int month, int day) {
+                        Vector ret = new Vector ();
+
+                       OpEvento op;
+                       ArrayList<Evento> e=null;
+                    try {
+                        op = new OpEvento();
+                        e= op.visualizzaElenco();
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                       
+                        for (Evento evento : e) {
+                            System.out.println(year + "/"+month + "/"+day + " " + evento.getData().getTimeInMillis() + " nuobo: " +
+                                   new GregorianCalendar(year, month, day).getTimeInMillis()
+                                    );
+                            if(evento.getData().get(GregorianCalendar.YEAR)==year && evento.getData().get(GregorianCalendar.MONTH)==month-1 && evento.getData().get(GregorianCalendar.DAY_OF_MONTH)==day)
+                                    {
+                                System.out.println("ok ci sto: " + evento.toString());
+                                ret.addElement ( new Event ( evento.getTema(),
+                evento.getLuogo(), year, month,  day, evento.getOra().get(GregorianCalendar.HOUR),
+                         evento.getOra().get(GregorianCalendar.MINUTE), evento.getOra().get(GregorianCalendar.SECOND)));
+                            }
+                        }
+                        return ret;
+                    }
+                });
+                panel.register(this);
+           
+
+
+            isPresente = true;
+            this.tabbedPane.addTab(s, panel);
+            System.out.println(this.tabbedPane.getTabCount() - 1);
+            ButtonTabComponent button = new ButtonTabComponent(this.tabbedPane, this.panel, this.menu);
+            this.tabbedPane.setTabComponentAt(this.tabbedPane.getTabCount() - 1, button);
+            panel.setVisible(true);
+
+
+
+            panel.repaint();
+            this.tabbedPane.setSelectedComponent(panel);
+            this.setEnabled(false);
+
+
+        }
+    }
+
+}
---
+++ seerp/src/us/k5n/ui/calendar/Event.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package us.k5n.ui.calendar;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author peppe
+ */
+public class Event implements EventInstance {
+    String title, description;
+		int Y, M, D, h, m, s;
+		boolean hasTime, allDay;
+		Color fg, bg, border;
+		Color[] colors = { Color.blue, Color.red, Color.orange, Color.pink,
+		    Color.gray, Color.green, Color.yellow, Color.cyan, Color.magenta };
+
+		public Event(String title, String description, int Y, int M, int D) {
+			this ( title, description, Y, M, D, 0, 0, 0, false, false );
+		}
+
+		public Event(String title, String description, int Y, int M, int D, int h,
+		    int m, int s) {
+			this ( title, description, Y, M, D, h, m, s, true, false );
+		}
+
+		public Event(String title, String description, int Y, int M, int D, int h,
+		    int m, int s, boolean hasTime, boolean allDay) {
+			this.title = title;
+			this.description = description;
+			this.Y = Y;
+			this.M = M;
+			this.D = D;
+			this.h = h;
+			this.m = m;
+			this.s = s;
+			this.hasTime = hasTime;
+			this.allDay = allDay;
+			// this.fg = new Color ( D * 8 % 256, h * 10 % 256, m * 4 % 256 );
+			this.bg = colors[ ( Y + M + D + h + m + s ) % colors.length];
+			this.fg = new Color ( this.bg.getRed () / 2, this.bg.getGreen () / 2,
+			    this.bg.getBlue () / 2 );
+			this.border = this.fg;
+		}
+
+		public String getTitle () {
+			return title;
+		}
+
+		public String getDescription () {
+			return description;
+		}
+
+		public boolean isAllDay () {
+			return allDay;
+		}
+
+		public boolean hasTime () {
+			return hasTime;
+		}
+
+		public int getYear () {
+			return Y;
+		}
+
+		public int getMonth () {
+			return M;
+		}
+
+		public int getDayOfMonth () {
+			return D;
+		}
+
+		public int getHour () {
+			return h;
+		}
+
+		public int getMinute () {
+			return m;
+		}
+
+		public int getSecond () {
+			return s;
+		}
+
+		public boolean hasDuration () {
+			return false;
+		}
+
+		public String getLocation ()
+		{
+			return null;
+		}
+
+		public int getDurationSeconds () {
+			return 0;
+		}
+
+		public Color getForegroundColor () {
+			return fg;
+		}
+
+		public Color getBackgroundColor () {
+			return bg;
+		}
+
+		public Color getBorderColor () {
+			return border;
+		}
+
+		/** Implement the Comparable interface so events can be sorted */
+		public int compareTo ( Object o ) {
+			EventInstance e2 = (EventInstance) o;
+			if ( this.getYear () < e2.getYear () )
+				return -1;
+			else if ( this.getYear () > e2.getYear () )
+				return 1;
+			if ( this.getMonth () < e2.getMonth () )
+				return -1;
+			else if ( this.getMonth () > e2.getMonth () )
+				return 1;
+			if ( this.getDayOfMonth () < e2.getDayOfMonth () )
+				return -1;
+			else if ( this.getDayOfMonth () > e2.getDayOfMonth () )
+				return 1;
+			if ( !this.hasTime && e2.hasTime () )
+				return -1;
+			else if ( this.hasTime () && !e2.hasTime () )
+				return 1;
+			else if ( !this.hasTime && !e2.hasTime () )
+				return 0;
+			if ( this.isAllDay () && !e2.isAllDay () )
+				return -1;
+			if ( !this.isAllDay () && e2.isAllDay () )
+				return 1;
+			if ( this.isAllDay () && e2.isAllDay () )
+				return 0;
+			// both events have a time
+			if ( this.getHour () < e2.getHour () )
+				return -1;
+			else if ( this.getHour () > e2.getHour () )
+				return 1;
+			if ( this.getMinute () < e2.getMinute () )
+				return -1;
+			else if ( this.getMinute () > e2.getMinute () )
+				return 1;
+			if ( this.getSecond () < e2.getSecond () )
+				return -1;
+			else if ( this.getSecond () > e2.getSecond () )
+				return 1;
+
+			return 0;
+		}
+}
